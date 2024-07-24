c781bd3562766881610c694a83d03537acd335be
Jeff Jewett <jewettje@oregonstate.edu>
2024-07-24 09:57:50 -0700

    gym wrapper utils

--- minimujo/utils/gym_wrapper.py
+++ minimujo/utils/gym_wrapper.py
@@ -1,12 +1,42 @@
+from typing import Type
 import gym
 import gym.spaces
 import gymnasium
 import numpy as np
 
-def gymnasium_box_to_gym(box: gymnasium.spaces.Box):
-    # gym Box doesn't like shape and low/high being vector
-    shape = box.shape if np.isscalar(box.low) else None
-    return gym.spaces.Box(box.low, box.high, shape, box.dtype)
+def gymnasium_space_to_gym(space: gymnasium.spaces.Space):
+    if isinstance(space, gymnasium.spaces.Box):
+        # gym Box doesn't like shape and low/high being vector
+        shape = space.shape if np.isscalar(space.low) else None
+        return gym.spaces.Box(space.low, space.high, shape=shape, dtype=space.dtype)
+    if isinstance(space, gymnasium.spaces.Discrete):
+        return gym.spaces.Discrete(space.n, start=space.start)
+    if isinstance(space, gymnasium.spaces.Dict):
+        spaces = {key:gymnasium_space_to_gym(val) for key, val in space.spaces.items()}
+        return gym.spaces.Dict(spaces)
+    if isinstance(space, gymnasium.spaces.Tuple):
+        spaces = tuple([gymnasium_space_to_gym(val) for val in space.spaces])
+        return gym.spaces.Tuple(spaces)
+    if isinstance(space, gymnasium.spaces.MultiBinary):
+        return gym.spaces.MultiBinary(space.n)
+    if isinstance(space, gymnasium.spaces.Text):
+        return gym.spaces.Text(max_length=space.max_length, min_length=space.min_length, charset=space.character_set)
+    # As a fallback, just return the unaltered space.
+    return space
+
+def unwrap_env(env: gym.Env, cls: Type):
+    if isinstance(env, GymnasiumToGymWrapper):
+            env = env._env
+    while env is not env.unwrapped:
+        if isinstance(env, cls):
+            return env
+        if isinstance(env, GymnasiumToGymWrapper):
+            env = env._env
+        else:
+            env = env.env
+    if isinstance(env, cls):
+            return env
+    return None
 
 class GymnasiumToGymWrapper(gym.Env):
 
@@ -14,8 +44,8 @@ class GymnasiumToGymWrapper(gym.Env):
         self._env = env
 
         self.metadata = env.metadata
-        self.observation_space = gymnasium_box_to_gym(env.observation_space)
-        self.action_space = gymnasium_box_to_gym(env.action_space)
+        self.observation_space = gymnasium_space_to_gym(env.observation_space)
+        self.action_space = gymnasium_space_to_gym(env.action_space)
 
     def reset(self, *args, **kwargs):
         obs, info = self._env.reset(*args, **kwargs)
@@ -27,3 +57,6 @@ class GymnasiumToGymWrapper(gym.Env):
     def step(self, action):
         obs, rew, term, trunc, info = self._env.step(action)
         return obs, rew, term or trunc, info
+    
+    def __getattr__(self, name):
+        return self._env.__getattr__(name)
