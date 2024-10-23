b2387d03031cedfeefd434f2447d4e2af2f837ea
Jeffrey Jewett <jewettje@oregonstate.edu>
2024-10-23 15:23:32 -0700

    drawing utils for goals

--- minimujo/utils/visualize/drawing.py
+++ minimujo/utils/visualize/drawing.py
@@ -2,17 +2,19 @@ from typing import Tuple
 import numpy as np
 import gymnasium as gym
 
-from minimujo.utils.gym_wrapper import unwrap_env
 from minimujo.dmc_gym import MinimujoGym
 from minimujo.box2d.gym import Box2DEnv
 
-def get_camera_bounds(env: gym.Env) -> Tuple[float,float,float,float]:
-    minimujo_env = unwrap_env(env, MinimujoGym)
-    if minimujo_env is None:
-        box2d_env = unwrap_env(env, Box2DEnv)
-        assert box2d_env is not None, f"Could not get coordinate bounds. {env} was not a MinimujoGym or Box2DEnv"
-        return 0, 0, box2d_env.arena_width, box2d_env.arena_height
-    return 0, 0, minimujo_env.arena.maze_width, minimujo_env.arena.maze_height
+def get_camera_bounds(env: gym.Env, norm_scale: bool = False) -> Tuple[float,float,float,float]:
+    unwrapped_env = env.unwrapped
+    if isinstance(unwrapped_env, MinimujoGym):
+        scale = unwrapped_env.arena.xy_scale if norm_scale else 1
+        return 0, -unwrapped_env.arena.maze_height / scale, unwrapped_env.arena.maze_width / scale, unwrapped_env.arena.maze_height / scale
+    if isinstance(unwrapped_env, Box2DEnv):
+        scale = unwrapped_env.xy_scale if norm_scale else 1
+        return 0, -unwrapped_env.arena_height / scale, unwrapped_env.arena_width / scale, unwrapped_env.arena_height / scale
+    else:
+        raise Exception(f"To get camera bounds, env must be a MinimujoGym or Box2DEnv. Actual type: {type(unwrapped_env)}")
 
 def draw_rectangle(
     image: np.ndarray, 
@@ -22,18 +24,21 @@ def draw_rectangle(
     color: np.ndarray,
     thickness: int
 ):
-    height, width = image.shape
+    cam_height, cam_width = image.shape[:2]
     min_x, min_y, bound_width, bound_height = bounds
-    screen1_x, screen1_y = (point1[0] - min_x) / bound_width, (point1[1] - min_y) / bound_height
-    screen2_x, screen2_y = (point2[0] - min_x) / bound_width, (point2[1] - min_y) / bound_height
+    screen1_x, screen1_y = int((point1[0] - min_x) / bound_width * cam_width), int((1-(point1[1] - min_y) / bound_height) * cam_height)
+    screen2_x, screen2_y = int((point2[0] - min_x) / bound_width * cam_height), int((1 - (point2[1] - min_y) / bound_height) * cam_height)
     topleft = max(0, min(screen1_x, screen2_x)), max(0, min(screen1_y, screen2_y))
-    botright = min(width-1, max(screen1_x, screen2_x)), min(height-1, max(screen1_y, screen2_y))
+    botright = min(cam_width-1, max(screen1_x, screen2_x)), min(cam_height-1, max(screen1_y, screen2_y))
 
+    # breakpoint()
     # draw horizontal top
     image[topleft[1]:topleft[1]+thickness, topleft[0]:botright[0]] = color
     # draw horizontal bottom
     image[botright[1]-thickness:botright[1], topleft[0]:botright[0]] = color
     # draw vertical left
-    image[topleft[1]:botright[1], topleft[0]:botright[0]+thickness] = color
+    image[topleft[1]:botright[1], topleft[0]:topleft[0]+thickness] = color
     # draw vertical right
-    image[topleft[1]:botright[1], topleft[0]-thickness:botright[0]] = color
+    image[topleft[1]:botright[1], botright[0]-thickness:botright[0]] = color
+
+    # image[0:100, 50:150] = color
