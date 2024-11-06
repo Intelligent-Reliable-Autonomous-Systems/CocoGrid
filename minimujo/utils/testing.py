824ed6b51a928d6cf0fcc3016caae3620eb4e586
Jeffrey Jewett <jewettje@oregonstate.edu>
2024-11-06 15:30:05 -0800

    testing utility for running environment

--- minimujo/utils/testing.py
+++ minimujo/utils/testing.py
@@ -42,3 +42,53 @@ def get_pygame_action():
     if keys[pygame.K_n] or keys[pygame.K_SPACE]:
         grab = 1
     return np.array([grab, -up, right])
+
+def run_gymnasium_env(env, print_reward=False, print_obs=False):
+    import pygame
+
+    obs, _ = env.reset()
+
+    print(f'Env has observation space {env.unwrapped.observation_space} and action space {env.unwrapped.action_space}')
+    print(f"Current task: {env.unwrapped.task}")
+
+    num_steps = 0
+    reward_sum = 0
+    num_episodes = 0
+    is_holding_reset = False
+    while True:
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_ESCAPE]:
+            print('Cumulative reward (to this point):', reward_sum)
+            print('Manually terminated')
+            break
+        if keys[pygame.K_r] and not is_holding_reset:
+            trunc = True
+            is_holding_reset = True
+        else:
+            if not keys[pygame.K_r]:
+                is_holding_reset = False
+            action = env.unwrapped.action_space.sample()
+            manual_action = get_pygame_action()
+            action[:3] = manual_action
+
+
+            obs, rew, term, trunc, info = env.step(action)
+            reward_sum += rew
+            num_steps += 1
+
+            if print_reward:
+                print('reward:', rew)
+
+            if print_obs:
+                print('obs:', obs)
+            
+        if term or trunc:
+            trunc_or_term = 'Truncated' if trunc else 'Terminated'
+            print('Cumulative reward:', reward_sum)
+            print(f'{trunc_or_term} after {num_steps} steps')
+            num_episodes += 1
+            env.reset()
+            reward_sum = 0
+            num_steps = 0
+            term = trunc = False
+            # print(f"Current task: {env.unwrapped.task}")
